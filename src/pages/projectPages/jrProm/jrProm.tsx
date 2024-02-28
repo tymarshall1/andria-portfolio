a3f4bfe27f9ac7787a692e1329d58d4f707e765c
tymarshall1 <60017345+tymarshall1@users.noreply.github.com>
2024-02-27 23:33:50 -0500

    compressed images a tiny bit to keep them loading quick

--- src/pages/projectPages/jrProm/jrProm.tsx
+++ src/pages/projectPages/jrProm/jrProm.tsx
@@ -13,10 +13,10 @@ function JrProm() {
     >
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_fill/v1709062593/Jr.%20Prom/nyvpx9b9nuidskrxgejr.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_fill/v1709062593/Jr.%20Prom/nyvpx9b9nuidskrxgejr.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_fill/v1709062593/Jr.%20Prom/nyvpx9b9nuidskrxgejr.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_fill/v1709062593/Jr.%20Prom/nyvpx9b9nuidskrxgejr.jpg"
         }
         desktopImg={""}
         altText={"prom picture one"}
@@ -25,10 +25,10 @@ function JrProm() {
 
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_fill/v1709062616/Jr.%20Prom/oxeflvtg5jr0dyln5cj9.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_fill/v1709062616/Jr.%20Prom/oxeflvtg5jr0dyln5cj9.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_fill/v1709062616/Jr.%20Prom/oxeflvtg5jr0dyln5cj9.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_fill/v1709062616/Jr.%20Prom/oxeflvtg5jr0dyln5cj9.jpg"
         }
         desktopImg={""}
         altText={"prom picture two"}
@@ -36,10 +36,10 @@ function JrProm() {
       ></Picture>
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_crop,g_north/v1709062704/Jr.%20Prom/im5qlmaxgghdxjzq47tz.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_crop,g_north/v1709062704/Jr.%20Prom/im5qlmaxgghdxjzq47tz.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_crop,g_north/v1709062704/Jr.%20Prom/im5qlmaxgghdxjzq47tz.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_crop,g_north/v1709062704/Jr.%20Prom/im5qlmaxgghdxjzq47tz.jpg"
         }
         desktopImg={""}
         altText={"prom picture six"}
@@ -48,10 +48,10 @@ function JrProm() {
 
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_16:10,c_fill/v1709062625/Jr.%20Prom/rrrospqvpfprppufzoby.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_16:10,c_fill/v1709062625/Jr.%20Prom/rrrospqvpfprppufzoby.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_12:10,c_fill/v1709062625/Jr.%20Prom/rrrospqvpfprppufzoby.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_12:10,c_fill/v1709062625/Jr.%20Prom/rrrospqvpfprppufzoby.jpg"
         }
         desktopImg={""}
         altText={"prom picture three"}
@@ -60,10 +60,10 @@ function JrProm() {
 
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_12:10,c_crop,g_north/v1709062664/Jr.%20Prom/s8qj98nampj8zrjhxlrz.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_12:10,c_crop,g_north/v1709062664/Jr.%20Prom/s8qj98nampj8zrjhxlrz.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_12:10,c_crop,g_north/v1709062664/Jr.%20Prom/s8qj98nampj8zrjhxlrz.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_12:10,c_crop,g_north/v1709062664/Jr.%20Prom/s8qj98nampj8zrjhxlrz.jpg"
         }
         desktopImg={""}
         altText={"prom picture four"}
@@ -72,10 +72,10 @@ function JrProm() {
 
       <Picture
         mobileImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_12:10,c_fill,g_auto/v1709062681/Jr.%20Prom/zbjksrohior2hbdfbgg1.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_12:10,c_fill,g_auto/v1709062681/Jr.%20Prom/zbjksrohior2hbdfbgg1.jpg"
         }
         tabletImg={
-          "https://res.cloudinary.com/dxwqyspsy/image/upload/ar_12:10,c_fill,g_auto/v1709062681/Jr.%20Prom/zbjksrohior2hbdfbgg1.jpg"
+          "https://res.cloudinary.com/dxwqyspsy/image/upload/q_30,ar_12:10,c_fill,g_auto/v1709062681/Jr.%20Prom/zbjksrohior2hbdfbgg1.jpg"
         }
         desktopImg={""}
         altText={"prom picture five"}
