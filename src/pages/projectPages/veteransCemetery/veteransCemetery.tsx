d8726fe706a2bc40a92ecca40389c080ba673962
tymarshall1 <60017345+tymarshall1@users.noreply.github.com>
2024-02-27 21:42:40 -0500

    adjusted grid row

--- src/pages/projectPages/veteransCemetery/veteransCemetery.tsx
+++ src/pages/projectPages/veteransCemetery/veteransCemetery.tsx
@@ -32,7 +32,7 @@ function VeteransCemetery() {
         }
         desktopImg={""}
         altText={"single fountain"}
-        css={""}
+        css={"lg:row-span-1"}
         imgCss="lg:h-full"
       ></Picture>
 
@@ -45,7 +45,7 @@ function VeteransCemetery() {
         }
         desktopImg={""}
         altText={"ducks in water"}
-        css={""}
+        css={"lg:row-span-1"}
         imgCss="lg:h-full"
       ></Picture>
 
@@ -93,7 +93,7 @@ function VeteransCemetery() {
         }
         desktopImg={""}
         altText={"flags at veterans cemetary"}
-        css={""}
+        css={"lg:row-span-1"}
         imgCss="lg:h-full"
       ></Picture>
     </BaseProjectPage>
